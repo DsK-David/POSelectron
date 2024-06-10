12fae511d3ea36f9f660a00a5a8586d0d40e806a
David silva <89320886+DsK-David@users.noreply.github.com>
2024-06-10 14:27:52 -0100

    melhorando carinho de produtos

--- src/js/main.js
+++ src/js/main.js
@@ -61,70 +61,93 @@
 // renderItems(); 
 // });
 
-function home(){
- const itemsContainer = document.getElementById("items");
- const homeButton=document.getElementById("home")
+function home() {
+  const itemsContainer = document.getElementById("items");
+  const homeButton = document.getElementById("home");
   const customersButton = document.getElementById("customers");
- const cartItemsContainer = document.getElementById("cart-items");
- const subtotalElement = document.getElementById("subtotal");
- const taxElement = document.getElementById("tax");
- const payableAmountElement = document.getElementById("payable-amount");
-// homeButton.style.border="1px solid #FC8019"
-// customersButton.style.border = "none";
- let cart = [];
- let itemsData = []; // Move itemsData para fora do evento para torná-la global
+  const cartItemsContainer = document.getElementById("cart-items");
+  const subtotalElement = document.getElementById("subtotal");
+  const taxElement = document.getElementById("tax");
+  const payableAmountElement = document.getElementById("payable-amount");
+  // homeButton.style.border="1px solid #FC8019"
+  // customersButton.style.border = "none";
+  let cart = [];
+  let itemsData = []; // Move itemsData para fora do evento para torná-la global
 
- async function fetchItemsData() {
-   try {
-     const response = await fetch("http://localhost:5500/api/v1/produtos"); // Substitua pela URL da sua API
-     itemsData = await response.json(); // Atualiza a variável global itemsData
-   } catch (error) {
-     console.error("Erro ao buscar dados da API:", error);
-   }
- }
+  async function fetchItemsData() {
+    try {
+      const response = await fetch("http://localhost:5500/api/v1/produtos"); // Substitua pela URL da sua API
+      itemsData = await response.json(); // Atualiza a variável global itemsData
+    } catch (error) {
+      console.error("Erro ao buscar dados da API:", error);
+    }
+  }
 
- async function renderItems() {
-   await fetchItemsData(); // Garante que itemsData seja preenchida antes de renderizar os itens
-   itemsContainer.innerHTML = "";
-   itemsData.forEach((item) => {
-     const itemDiv = document.createElement("div");
-     itemDiv.className = "item";
-     itemDiv.innerHTML = `<img src="${item.imagem}" alt="${item.nome}">
+  async function renderItems() {
+    await fetchItemsData(); // Garante que itemsData seja preenchida antes de renderizar os itens
+    itemsContainer.innerHTML = "";
+    itemsData.forEach((item) => {
+      const itemDiv = document.createElement("div");
+      itemDiv.className = "item";
+      itemDiv.innerHTML = `<img src="${item.imagem}" alt="${item.nome}">
       <br><span>${item.nome}</span>
       <br><strong>$${item.preco}</strong>`;
-     itemDiv.onclick = () => addToCart(item); // Passa o objeto item diretamente
-     itemsContainer.appendChild(itemDiv);
-   });
- }
+      itemDiv.onclick = () => addToCart(item); // Passa o objeto item diretamente
+      itemsContainer.appendChild(itemDiv);
+    });
+  }
 
- function addToCart(item) {
-   cart.push(item); // Adiciona o item diretamente ao carrinho
-   renderCart();
-   scrollToBottom(cartItemsContainer);
- }
+  function addToCart(item) {
+    cart.push(item); // Adiciona o item diretamente ao carrinho
+    renderCart();
+    scrollToBottom(cartItemsContainer);
+  }
+  document
+    .getElementById("cart-items")
+    .addEventListener("click", function (event) {
+      if (event.target.classList.contains("remove-item")) {
+        const index = parseInt(event.target.getAttribute("data-index"));
+        if(!isNaN(index)){
 
- function renderCart() {
-   cartItemsContainer.innerHTML = "";
-   let subtotal = 0;
-   cart.forEach((item) => {
-     const cartItemDiv = document.createElement("div");
-     cartItemDiv.innerText = `${item.nome} - $${item.preco} x`;
-     cartItemsContainer.appendChild(cartItemDiv);
-     subtotal += item.preco;
-   });
-   const tax = subtotal * 0.225;
-   const payableAmount = subtotal - tax;
+          cart.splice(index, 1); // Remove o item do carrinho
+          renderCart(); // Re-renderiza o carrinho após a remoção
+        }
+      }
+    });
+  function renderCart() {
+    cartItemsContainer.innerHTML = "";
+    
+    let subtotal = 0;
+    cart.forEach((item,index) => {
+      const cartItemDiv = document.createElement("div");
+      cartItemDiv.className = "cart-item";
+      cartItemDiv.innerHTML = `
+      <div class="item-details">
+          <span>1</span> ${item.nome}
+          <span class="item-price">$${item.preco}</span>
+        </div>
+        <button class="remove-item" data-index="${index}"">✕</button>
+        `;
+        
+      cartItemsContainer.appendChild(cartItemDiv);
+      subtotal += item.preco;
+    });
+    const tax = subtotal * 0.225;
+    const payableAmount = subtotal - tax;
 
-   subtotalElement.innerText = subtotal.toFixed(2);
-   taxElement.innerText = tax.toFixed(2);
-   payableAmountElement.innerText = payableAmount.toFixed(2);
- }
+    subtotalElement.innerText = subtotal.toFixed(2);
+    taxElement.innerText = tax.toFixed(2);
+    payableAmountElement.innerText = payableAmount.toFixed(2);
 
- function scrollToBottom(container) {
-   container.scrollTop = container.scrollHeight;
- }
- // Chama renderItems após o evento DOMContentLoaded
- renderItems(); 
+
+scrollToBottom(cartItemsContainer)
+  }
+
+  function scrollToBottom(container) {
+    container.scrollTop = container.scrollHeight;
+  }
+  // Chama renderItems após o evento DOMContentLoaded
+  renderItems();
 }
 document.querySelectorAll(".sidebar button").forEach((button) => {
   button.addEventListener("click", function () {
@@ -136,16 +159,15 @@ document.querySelectorAll(".sidebar button").forEach((button) => {
     this.classList.add("active");
   });
 });
-function clientes(){
+function clientes() {
   const homeButton = document.getElementById("home");
-    const customersButton = document.getElementById("customers");
-    customersButton.className = "select";
-    // homeButton.style.border = "none";
-    // customersButton.style.border = "1px solid #FC8019";
+  const customersButton = document.getElementById("customers");
+  customersButton.className = "select";
+  // homeButton.style.border = "none";
+  // customersButton.style.border = "1px solid #FC8019";
 
-    
-    const itemsContainer = document.getElementById("items");
-    itemsContainer.innerHTML = `
+  const itemsContainer = document.getElementById("items");
+  itemsContainer.innerHTML = `
     <div class="cliente_content"> 
     <form action="/adicionar_cliente" method="POST" class="cliente-formulario">
     <h2>Adicionar Cliente</h2>
@@ -170,7 +192,6 @@ function clientes(){
 
     
     `;
-    
 }
 function cashier() {
   const homeButton = document.getElementById("home");
