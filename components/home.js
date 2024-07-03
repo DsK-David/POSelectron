86ffc5a5acb8d2f5a6f34cbccb509d08246ccc52
David silva <89320886+DsK-David@users.noreply.github.com>
2024-07-03 14:51:29 -0100

    adicionando a função comprar no arquivo

--- components/home.js
+++ components/home.js
@@ -74,5 +74,59 @@ function removeCartItem(index) {
 function scrollToBottom(container) {
   container.scrollTop = container.scrollHeight;
 }
+let clienteNaFatura = null
+function adicionarNaFatura(user) {
+  const cliente_header = document.querySelector(".cliente_na_fatura");
+  const cart_header = document.querySelector(".cart-header")
+  cliente_header.innerHTML = `
+    <span>${user.nome}</span> - <span>${user.email}</span> - <span>${user.obs}</span>
+    <button class="remove-item">X</button>`;
 
-export { renderItems, addToCart, renderCart, scrollToBottom };
+  // Adiciona um ouvinte de evento para remover o item do carrinho
+  cliente_header.querySelector('.remove-item').addEventListener('click', () => {
+    cliente_header.parentElement.removeChild(cliente_header);
+    clienteNaFatura=null
+  });
+  clienteNaFatura=user
+}
+async function comprar(){
+    if (cart.length === 0) {
+        alert('Carrinho vazio. Nenhum item para enviar.');
+        return; // Sai da função se o carrinho estiver vazio
+    }
+
+    const itemsToSend = cart.map(item => ({
+        nome: item.nome,
+        preco: item.preco
+    }))
+
+    try {
+        const response = await fetch("http://localhost:5500/api/v1/compra", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            itens: JSON.stringify(itemsToSend),
+            total: cart.reduce((acc, item) => acc + item.preco, 0), // Supondo que 'preco' é o preço de cada item
+            codigo: "SEU_CODIGO_DA_COMPRA", // Substitua 'SEU_CODIGO_DA_COMPRA' pelo código real da compra
+            data_compra: new Date().toISOString(), // Usa a data atual como string ISO
+            hora_compra: new Date().toLocaleTimeString(), // Usa a hora local atual como string
+            cliente:clienteNaFatura?{
+              nome:clienteNaFatura.nome,
+              email:clienteNaFatura.email,
+              obs:clienteNaFatura.obs
+            }:null,
+          }),
+        });
+
+        if (!response.ok) throw new Error('Falha ao enviar os itens para checkout');
+
+        alert('Itens enviados com sucesso!');
+        cart = []; // Limpa o carrinho após o envio bem-sucedido
+        renderCart(); // Atualiza a visualização do carrinho, se necessário
+    } catch (error) {
+        console.error('Erro ao enviar os itens:', error);
+    }
+}
+export { renderItems, addToCart, renderCart, scrollToBottom,comprar };
